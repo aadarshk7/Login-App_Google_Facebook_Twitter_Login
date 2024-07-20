4ca18ae84ee88136259ca66f186d65e0d24e10eb
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-20 09:52:44 +0545

    Login_App

---
+++ lib/auth_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthService with ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? get currentUser => _auth.currentUser;
+
+  Stream<User?> get user {
+    return _auth.authStateChanges();
+  }
+
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      print(e.toString());
+    }
+    notifyListeners();
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+    notifyListeners();
+  }
+
+  Future<void> signUp(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      print(e.toString());
+    }
+    notifyListeners();
+  }
+}
