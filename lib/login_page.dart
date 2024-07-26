588de999ed54695a8a567258e75aa3288869e75d
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-26 22:39:18 +0545

    Login_App

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -20,42 +20,41 @@ class _LoginPageState extends State<LoginPage> {
         title: Text('Login'),
 
       ),
-      // appBar: AppBar(title: Text('Login')),
-      // body: Padding(
-      //   padding: const EdgeInsets.all(16.0),
-      //   child: Column(
-      //     children: [
-      //       TextField(
-      //         controller: emailController,
-      //         decoration: InputDecoration(labelText: 'Email'),
-      //       ),
-      //       TextField(
-      //         controller: passwordController,
-      //         decoration: InputDecoration(labelText: 'Password'),
-      //         obscureText: true,
-      //       ),
-      //       SizedBox(height: 20),
-      //       ElevatedButton(
-      //         onPressed: () async {
-      //           await authService.signIn(
-      //             emailController.text,
-      //             passwordController.text,
-      //           );
-      //         },
-      //         child: Text('Login'),
-      //       ),
-      //       ElevatedButton(
-      //         onPressed: () async {
-      //           await authService.signUp(
-      //             emailController.text,
-      //             passwordController.text,
-      //           );
-      //         },
-      //         child: Text('Sign Up'),
-      //       ),
-      //     ],
-      //   ),
-      // ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                await authService.signIn(
+                  emailController.text,
+                  passwordController.text,
+                );
+              },
+              child: Text('Login'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await authService.signUp(
+                  emailController.text,
+                  passwordController.text,
+                );
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
