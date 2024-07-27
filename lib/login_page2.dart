d935a6b19ed1d35968b0a2f7457037599a96e3b9
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-07-27 09:39:14 +0545

    Login_App

--- lib/login_page2.dart
+++ lib/login_page2.dart
@@ -1,6 +1,11 @@
-class LoginPage2 extends StatelessWidget {
+class LoginPage2 extends StatefulWidget {
   const LoginPage2({super.key});
 
+  @override
+  State<LoginPage2> createState() => _LoginPage2State();
+}
+
+class _LoginPage2State extends State<LoginPage2> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
