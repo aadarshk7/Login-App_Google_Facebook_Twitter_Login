1a74c0241678549059c71654a9d38e9f1666e0db
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-02 12:37:31 +0545

    Login_App

--- lib/provider/sign_in_provider.dart
+++ lib/provider/sign_in_provider.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../utils/config.dart';
+
 class SignInProvider extends ChangeNotifier {
+  // instance of firebaseauth, facebook and google
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final FacebookAuth facebookAuth = FacebookAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  // final twitterLogin = TwitterLogin(
+  //   //     apiKey: Config.apikey_twitter,
+  //   //     apiSecretKey: Config.secretkey_twitter,
+  //   //     redirectURI: "socialauth://");
+
   bool _isSignIn = false;
   bool get isSignIn => _isSignIn;
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import flutter_secure_storage_macos
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
+import twitter_login
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -25,4 +26,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  TwitterLoginPlugin.register(with: registry.registrar(forPlugin: "TwitterLoginPlugin"))
 }
