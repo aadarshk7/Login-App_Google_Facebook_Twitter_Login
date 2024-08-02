679bbc1414a18df50920794db657989ca140d36a
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-02 12:44:22 +0545

    Login_App

--- lib/provider/sign_in_provider.dart
+++ lib/provider/sign_in_provider.dart
@@ -18,29 +18,38 @@ class SignInProvider extends ChangeNotifier {
   //   //     redirectURI: "socialauth://");
 
   bool _isSignIn = false;
+
   bool get isSignIn => _isSignIn;
 
   //hasError, errorCode, provider,uid, email, name, imageUrl
   bool _hasError = false;
+
   bool get hasError => _hasError;
 
   String? _errorCode;
+
   String? get errorCode => _errorCode;
 
   String? _provider;
+
   String? get provider => _provider;
 
   String? _uid;
+
   String? get uid => _uid;
 
   String? _name;
+
   String? get name => _name;
 
   String? _email;
+
   String? get email => _email;
 
   String? _imageUrl;
+
   String? get imageUrl => _imageUrl;
+
   SignInProvider() {
     checkSignInUser();
   }
