427c4bbeee3a908a696ed3065e38fa1016075dd2
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-08-02 12:33:41 +0545

    Login_App

--- lib/provider/sign_in_provider.dart
+++ lib/provider/sign_in_provider.dart
@@ -4,6 +4,28 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SignInProvider extends ChangeNotifier {
   bool _isSignIn = false;
   bool get isSignIn => _isSignIn;
+
+  //hasError, errorCode, provider,uid, email, name, imageUrl
+  bool _hasError = false;
+  bool get hasError => _hasError;
+
+  String? _errorCode;
+  String? get errorCode => _errorCode;
+
+  String? _provider;
+  String? get provider => _provider;
+
+  String? _uid;
+  String? get uid => _uid;
+
+  String? _name;
+  String? get name => _name;
+
+  String? _email;
+  String? get email => _email;
+
+  String? _imageUrl;
+  String? get imageUrl => _imageUrl;
   SignInProvider() {
     checkSignInUser();
   }
